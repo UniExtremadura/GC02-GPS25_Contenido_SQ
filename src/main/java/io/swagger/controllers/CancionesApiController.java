0a2befad97f9103e847a216a4547f50e1942b26a
uni-msg <msolisz@alumnos.unex.es>
2025-11-21 18:43:34 +0100

    GPS25-141 Desarrollo del endpoint #time 2h

--- src/main/java/io/swagger/controllers/CancionesApiController.java
+++ src/main/java/io/swagger/controllers/CancionesApiController.java
@@ -1,145 +1,128 @@
 package io.swagger.controllers;
 
+import io.swagger.api.CancionesApi;
+import io.swagger.entity.CancionEntity;
+import io.swagger.entity.ElementoEntity;
 import io.swagger.model.Cancion;
-import io.swagger.model.CancionInput;
-import io.swagger.model.ErrorResponse;
-import io.swagger.api.*;
+import io.swagger.services.CancionService;
+import io.swagger.services.ElementoService;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
-import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
+
+
+import io.swagger.model.CancionInput;
+import io.swagger.model.CancionPut;
 
 import javax.validation.Valid;
-import javax.validation.constraints.*;
+import javax.validation.constraints.Null;
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
 import java.util.List;
-import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2025-11-10T17:11:09.236506587Z[GMT]")
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2025-10-27T17:33:52.662194674Z[GMT]")
 @RestController
 public class CancionesApiController implements CancionesApi {
 
     private static final Logger log = LoggerFactory.getLogger(CancionesApiController.class);
-
     private final ObjectMapper objectMapper;
-
     private final HttpServletRequest request;
+    private final CancionService cancionService;
+    private final ElementoService elementoService;
 
     @org.springframework.beans.factory.annotation.Autowired
-    public CancionesApiController(ObjectMapper objectMapper, HttpServletRequest request) {
+    public CancionesApiController(ObjectMapper objectMapper, HttpServletRequest request, CancionService cancionService, ElementoService elementoService) {
         this.objectMapper = objectMapper;
         this.request = request;
+        this.cancionService = cancionService;
+        this.elementoService = elementoService;
     }
 
-    public ResponseEntity<List<Cancion>> cancionesAlbumIdAlbumGet(@Parameter(in = ParameterIn.PATH, description = "ID del álbum cuyas canciones se desean consultar", required=true, schema=@Schema()) @PathVariable("idAlbum") Integer idAlbum
-) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<List<Cancion>>(objectMapper.readValue("[ \"\", \"\" ]", List.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<List<Cancion>>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
-
-        return new ResponseEntity<List<Cancion>>(HttpStatus.NOT_IMPLEMENTED);
+    @GetMapping("/canciones/album/{idAlbum}")
+    public ResponseEntity<List<Cancion>> cancionesAlbumIdAlbumGet(@Parameter(in = ParameterIn.PATH, description = "ID del álbum cuyas canciones se desean consultar", required=true, schema=@Schema()) @PathVariable("idAlbum") Integer idAlbum) {
+        // todo: implementar método
+        return null;
     }
 
-    public ResponseEntity<List<Cancion>> cancionesArtistaIdArtistaGet(@Parameter(in = ParameterIn.PATH, description = "ID del artista cuyas canciones se desean consultar", required=true, schema=@Schema()) @PathVariable("idArtista") Integer idArtista
-) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<List<Cancion>>(objectMapper.readValue("[ \"\", \"\" ]", List.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<List<Cancion>>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
-
-        return new ResponseEntity<List<Cancion>>(HttpStatus.NOT_IMPLEMENTED);
+    @GetMapping("/canciones/artista/{idArtista}")
+    public ResponseEntity<List<Cancion>> cancionesArtistaIdArtistaGet(@Parameter(in = ParameterIn.PATH, description = "ID del artista cuyas canciones se desean consultar", required=true, schema=@Schema()) @PathVariable("idArtista") Integer idArtista) {
+       // todo: implementar método
+        return null;
     }
 
-    public ResponseEntity<List<Cancion>> cancionesGeneroIdGeneroGet(@Parameter(in = ParameterIn.PATH, description = "ID del género cuyas canciones se desean consultar", required=true, schema=@Schema()) @PathVariable("idGenero") Integer idGenero
-) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<List<Cancion>>(objectMapper.readValue("[ \"\", \"\" ]", List.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<List<Cancion>>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
-
-        return new ResponseEntity<List<Cancion>>(HttpStatus.NOT_IMPLEMENTED);
+    @GetMapping("/canciones/genero/{idGenero}")
+    public ResponseEntity<List<Cancion>> cancionesGeneroIdGeneroGet(@Parameter(in = ParameterIn.PATH, description = "ID del género cuyas canciones se desean consultar", required=true, schema=@Schema()) @PathVariable("idGenero") Integer idGenero) {
+       // todo: implementar método
+        return null;
     }
+        
 
+    @Override
     public ResponseEntity<List<Cancion>> cancionesGet(@Parameter(in = ParameterIn.QUERY, description = "ID del álbum al que pertenece la canción" ,schema=@Schema()) @Valid @RequestParam(value = "idAlbum", required = false) Integer idAlbum
-,@Parameter(in = ParameterIn.QUERY, description = "Nombre de la canción" ,schema=@Schema()) @Valid @RequestParam(value = "nombre", required = false) String nombre
-) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<List<Cancion>>(objectMapper.readValue("[ \"\", \"\" ]", List.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<List<Cancion>>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
-
-        return new ResponseEntity<List<Cancion>>(HttpStatus.NOT_IMPLEMENTED);
+            ,@Parameter(in = ParameterIn.QUERY, description = "Nombre de la canción" ,schema=@Schema()) @Valid @RequestParam(value = "nombre", required = false) String nombre) {
+       // todo: implementar método
+        return null;
+    
     }
 
-    public ResponseEntity<CancionInput> cancionesPost(@Parameter(in = ParameterIn.DEFAULT, description = "", required=true, schema=@Schema()) @Valid @RequestBody CancionInput body
-) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<CancionInput>(objectMapper.readValue("\"\"", CancionInput.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<CancionInput>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
+    @Override
+    public ResponseEntity<Cancion> cancionesPost(@Parameter(in = ParameterIn.DEFAULT, description = "", required=true, schema=@Schema()) @Valid @RequestBody CancionInput body) {
+        // 1. Crear Elemento
+        ElementoEntity elemento = new ElementoEntity();
+        elemento.setNombre(body.getNombre());
+        elemento.setDescripcion(body.getDescripcion());
+        elemento.setPrecio(body.getPrecio());
+        elemento.setNumventas(0);
+        elemento.setValoracion(0);
+        elemento.setEsnovedad(false);
+        elemento.setEsalbum(false); // Es canción
+        elemento.setArtista(body.getArtista());
+        elemento.setGenero(body.getGenero());
+        elemento.setSubgenero(body.getSubgenero());
+        elemento.setUrlFoto(body.getUrlFoto());
+
+        // Guardar elemento → genera ID
+        elemento = elementoService.save(elemento);
+        System.out.println("Elemento creado con ID: " + elemento.getId());
+
+        // 2. Crear Canción con el mismo ID
+        CancionEntity cancion = new CancionEntity();
+        cancion.setElemento(elemento);        // Relación
+        cancion.setNombreAudio(body.getNombreAudio());
+        cancion.setNumRep(body.getNumRep());
+
+        // Si tiene álbum
+        if (body.getIdAlbum() != null) {
+            ElementoEntity album = elementoService.getByIdOrThrow(body.getIdAlbum());
+            cancion.setAlbum(album);
+        }
+        else {
+            cancion.setAlbum(null);
         }
 
-        return new ResponseEntity<CancionInput>(HttpStatus.NOT_IMPLEMENTED);
+
+        // Guardar canción
+        cancion = cancionService.save(cancion);
+
+        // Convertir a modelo Swagger
+        return ResponseEntity.status(HttpStatus.CREATED).body(cancionService.convertToInputModel(cancion));
     }
 
-    public ResponseEntity<CancionInput> cancionesPut(@Parameter(in = ParameterIn.DEFAULT, description = "", required=true, schema=@Schema()) @Valid @RequestBody CancionInput body
-) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<CancionInput>(objectMapper.readValue("\"\"", CancionInput.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<CancionInput>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
-
-        return new ResponseEntity<CancionInput>(HttpStatus.NOT_IMPLEMENTED);
+    @Override
+    public ResponseEntity<Cancion> cancionesPut(@Parameter(in = ParameterIn.DEFAULT, description = "", required=true, schema=@Schema()) @Valid @RequestBody CancionPut body) {
+        //  todo: implementar actualización de canción
+        return null;
     }
-
 }
