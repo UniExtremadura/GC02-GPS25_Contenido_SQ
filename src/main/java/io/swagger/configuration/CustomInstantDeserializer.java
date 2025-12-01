5727587ac50ae6db462a669ae2b281b2393f46ea
RinaHodge <rhodgefo@alumnos.unex.es>
2025-12-01 21:50:10 +0100

    Resuelta incidencia 3 (Arreglado NotundefinedValidator.java)

--- src/main/java/io/swagger/configuration/CustomInstantDeserializer.java
+++ src/main/java/io/swagger/configuration/CustomInstantDeserializer.java
@@ -199,8 +199,9 @@ public class CustomInstantDeserializer<T extends Temporal>
         }
         return value;
       }
+      default:
+        throw context.mappingException("Expected type float, integer, or string.");
     }
-    throw context.mappingException("Expected type float, integer, or string.");
   }
 
   private ZoneId getZone(DeserializationContext context) {
